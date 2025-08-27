63030ba1345132d996c1a0e0d269f8d63568d805
Anatoli <64985361+DaDe287@users.noreply.github.com>
2025-08-27 22:57:40 +0300

    Create install-outline.sh

---
+++ install-outline.sh
@@ -0,0 +1,9 @@
+clear
+sudo apt update && sudo apt upgrade -y
+sudo ufw allow 80/tcp
+sudo ufw allow 8080/tcp
+sudo ufw allow 22/tcp
+sudo ufw allow 1024:65535/udp
+sudo ufw allow 1024:65535/tcp
+echo "y" | sudo ufw enable
+echo "Y" | sudo wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash
