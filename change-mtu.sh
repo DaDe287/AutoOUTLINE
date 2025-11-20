1c0c2925539e5f130fa6658e7225a60985506fe1
Anatoli <64985361+DaDe287@users.noreply.github.com>
2025-11-20 22:26:56 +0300

    Update change-mtu.sh

--- change-mtu.sh
+++ change-mtu.sh
@@ -3,7 +3,7 @@ sudo ip link set dev eth0 mtu 1000
 echo "      mtu 1000" > /etc/network/interfaces
 sudo systemctl restart networking
 
-echo "{ "mtu": 1000 }" > /etc/docker/daemon.json
+echo "{ 'mtu': 1000 }" > /etc/docker/daemon.json
 sudo systemctl daemon-reload
 
 sudo systemctl restart docker
